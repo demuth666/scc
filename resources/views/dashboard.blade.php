03408a4c82315a5273e31b25a501a5a0296e4d64
Ghifarinugraha <ghifatycoon@gmail.com>
2024-12-02 09:12:09 +0700

    add: landingPage

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,5 +1,75 @@
 <x-app-layout>
-    <div class="py-1">
-        <img class="h-auto max-w-full blur-sm" src="{{asset('images/SCC_building.png')}}" alt="image scc"/>
-    </div>
+
+    <!-- Hero Section -->
+    <section class="flex flex-col min-h-screen bg-slate-600 text-white bg-center bg-cover bg-blend-overlay bg-black/30 " style="background-image:url({{asset('images/SCC_building.png')}});">
+        <div class="flex-1 flex items-center">
+            <div class="text-center mx-auto">
+                <h1 class="sm:text-5xl text-2xl px-4 font-semibold">Penuhi Kebutuhan Kreatifmu di <br> <span class="text-orange-400">Sumedang Creative Center</span> </h1>
+            </div>
+        </div>
+    </section>
+
+    <!-- content section -->
+    <section class="h-screen">
+        <!-- content -->
+        <div class="container p-2 max-w-md mx-auto sm:max-w-xl sm:max-w-full lg:max-w-full">
+            <div class=" shadow-1lg p-6 ">
+                <!-- Pengenalan SCC -->
+                <div class="flex">
+                    <div class="lg:block lg:flex lg:w-1/2 hidden">
+                       <img class=" rounded-xl" src="{{asset('images/scc_building.png')}}" alt="sccimg">
+                   </div>
+                   <div class="lg:flex-1 lg:p-8">
+                       <h2 class="text-2xl font-bold">Apa itu SCC?</h2>
+                       <img class="mt-2 rounded-xl lg:hidden sm:w-full" src="{{asset('images/scc_building.png')}}" alt="sccimg">
+                       <p class="text-sm mt-4">Sumedang Crative Center (SCC) adalah pusat kreativitas di Kabupaten Sumedang yang didedikasikan untuk mendukung perkembangan seni, budaya dan inovasi. Dengan fasilitas modern seperti ruang pertemuan, studio kreatif, ruang pameran, ruang editing, area diskusi dan ruang lainnya, SCC memberikan tempat yang nyaman dan inspiratif untuk berbagai kegiatan mulai dari workshop, pameran seni, hingga acara komunitas.</p>
+                       <div class="mt-6">
+                           <a href="/" class=" px-5 py-3 rounded-lg  text-white font-semibold bg-orange-400 hover:bg-orange-500 transition ease-in-out">Selengkapnya</a>
+                       </div>
+                   </div>    
+                </div>
+                <!-- Pengenalan SCC END -->
+
+                <!-- Card Ruangan -->
+                <div class="mt-8 ">
+                    <h2 class="text-2xl font-bold">Ruangan yang tersedia</h2>
+                </div>
+                <div class="container">
+                    <div class="swiper">
+                        <div class="swiper-wrapper">
+                            <div class="mx-auto rounded-xl bg-base-100 w-72 shadow-lg my-4">
+                                <figure>
+                                    <img class="rounded"
+                                    src="{{asset('images/scc_building.png')}}"
+                                    alt="Ruang Kelas" />
+                                </figure>
+                                <div class="p-4">
+                                    <h2 class="font-semibold text-lg">Ruang Kelas</h2>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                                    <button class="mt-2 px-5 py-2 rounded-lg  text-white font-semibold bg-orange-400 hover:bg-orange-500 transition ease-in-out"><a href="">Booking</a></button>
+                                </div>
+                            </div>
+                            <div class="mx-auto rounded-xl bg-base-100 w-72 shadow-lg my-4">
+                                <figure>
+                                    <img class="rounded"
+                                    src="{{asset('images/scc_building.png')}}"
+                                    alt="Ruang Kelas" />
+                                </figure>
+                                <div class="p-4">
+                                    <h2 class="font-semibold text-lg">Ruang Kelas</h2>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                                    <button class="mt-2 px-5 py-2 rounded-lg  text-white font-semibold bg-orange-400 hover:bg-orange-500 transition ease-in-out"><a href="">Booking</a></button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div >
+                
+                <!-- Card Ruangan END -->
+
+            </div>
+        </div>
+        <!-- Content END -->
+    </section>
+
 </x-app-layout>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,18 +4,32 @@ import forms from '@tailwindcss/forms';
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
     ],
+
+    daisyui: {
+        themes: false, // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
+        darkTheme: false, // name of one of the included themes for dark mode
+        base: true, // applies background color and foreground color for root element by default
+        styled: true, // include daisyUI colors and design decisions for all components
+        utils: true, // adds responsive and modifier utility classes
+        prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
+        logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
+        themeRoot: ":root", // The element that receives theme color CSS variables
+    },
 
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ["Figtree", ...defaultTheme.fontFamily.sans],
+            },
+            boxShadow: {
+                "1lg": "0px 0px 15px 3px rgba(0,0,0,0.1)",
             },
         },
     },
 
-    plugins: [forms],
+    plugins: [require("daisyui")],
 };
